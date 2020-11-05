8d0b45563a103e9ff45f0eed8a181bd1c951463f
adam <mr.adamtyler@gmail.com>
2020-11-05 21:50:18 +0000

    Update error messages. Lint code.

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,20 +2,19 @@ const request = require('request');
 
 const breed = process.argv[2];
 const breedsAPI = 'https://api.thecatapi.com/v1/breeds/search';
-// const breedsAPI = 'https://api.thecratapi.com/v1/breeds/search';
 const callQuery = `${breedsAPI}?q=${breed}`;
 
 request(callQuery, (err, response, body) => {
   if (err) {
     console.log(err);
     console.log();
-    console.log(`Requested hostname: ${err.hostname}. Full error message above.`);
+    console.log(`ERROR:: Requested hostname: ${err.hostname}. Full error message above.`);
   } else if (body === `[]`) {
-    console.log(`Requested breed not found: '${breed}'. Usage: \`node breedFetcher.js [breed]\``);
+    console.log(`ERROR:: Requested breed not found: '${breed}'.\nUsage: \`node breedFetcher.js [breed]\``);
   } else {
     const data = JSON.parse(body);
-    console.log('data:::', data);
-    // console.log(`body.length:::`, body.length)
-    // console.log(data[0].name, ',',data[0].origin);
+    // console.log('data:::', data);
+    console.log(`body.length:::`, body.length);
+    console.log(data[0].name, ',',data[0].origin);
   }
 });
