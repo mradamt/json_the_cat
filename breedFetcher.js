b4b093bb8a6691e91c14ee4e9b308b44bc007e5a
adam <mr.adamtyler@gmail.com>
2020-11-05 21:48:32 +0000

    Handle error cases

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,14 +2,20 @@ const request = require('request');
 
 const breed = process.argv[2];
 const breedsAPI = 'https://api.thecatapi.com/v1/breeds/search';
+// const breedsAPI = 'https://api.thecratapi.com/v1/breeds/search';
 const callQuery = `${breedsAPI}?q=${breed}`;
 
 request(callQuery, (err, response, body) => {
-  if (err) throw err;
-  // console.log(body)
-  // console.log(typeof body)
-  const data = JSON.parse(body);
-  // console.log(data);
-  // console.log(typeof data);
-  console.log(data[0].description);
+  if (err) {
+    console.log(err);
+    console.log();
+    console.log(`Requested hostname: ${err.hostname}. Full error message above.`);
+  } else if (body === `[]`) {
+    console.log(`Requested breed not found: '${breed}'. Usage: \`node breedFetcher.js [breed]\``);
+  } else {
+    const data = JSON.parse(body);
+    console.log('data:::', data);
+    // console.log(`body.length:::`, body.length)
+    // console.log(data[0].name, ',',data[0].origin);
+  }
 });
