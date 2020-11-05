a3038cdf16193e3884e97b43f36c68acc1f312fc
adam <mr.adamtyler@gmail.com>
2020-11-05 22:14:17 +0000

    Refactor function to run from index.js; implement tests

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,20 +1,20 @@
 const request = require('request');
 
-const breed = process.argv[2];
 const breedsAPI = 'https://api.thecatapi.com/v1/breeds/search';
-const callQuery = `${breedsAPI}?q=${breed}`;
 
-request(callQuery, (err, response, body) => {
-  if (err) {
-    console.log(err);
-    console.log();
-    console.log(`ERROR:: Requested hostname: ${err.hostname}. Full error message above.`);
-  } else if (body === `[]`) {
-    console.log(`ERROR:: Requested breed not found: '${breed}'.\nUsage: \`node breedFetcher.js [breed]\``);
-  } else {
-    const data = JSON.parse(body);
-    // console.log('data:::', data);
-    console.log(`body.length:::`, body.length);
-    console.log(data[0].name, ',',data[0].origin);
-  }
-});
+const fetchBreedDescription = function(breedName, callback) {
+  const callQuery = `${breedsAPI}?q=${breedName}`;
+  request(callQuery, (err, response, body) => {
+    if (err) {
+      callback(err, null);
+    } else if (body === `[]`) {
+      const errorMsg = `ERROR:: Requested breed not found: '${breedName}'.\nUsage: \`node breedFetcher.js [breed]\``;
+      callback(errorMsg, null);
+    } else {
+      const data = JSON.parse(body);
+      callback(null, data[0].description);
+    }
+  });
+};
+
+module.exports = { fetchBreedDescription };
